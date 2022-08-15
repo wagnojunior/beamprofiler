18f476ab17d1911d89ff4dae04995df486ac66de
wagnojunior <wagnojunior@gmail.com>
2022-08-15 17:06:23 +0900

    Bump version: 0.1.4 → 1.0.0

--- setup.py
+++ setup.py
@@ -61,6 +61,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/wagnojunior/beamprofiler',
-    version='0.1.4',
+    version='1.0.0',
     zip_safe=False,
 )
