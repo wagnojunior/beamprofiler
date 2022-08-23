db15fd5b81161f33366b7a063f6dfacc0e574b55
Wagno <wagnojunior@gmail.com>
2022-08-23 20:44:50 +0900

    Improve import statement

--- src/beamprofiler/niso/__init__.py
+++ src/beamprofiler/niso/__init__.py
@@ -1,3 +1,10 @@
 """
 This package handles the `non-ISO` part of the beam analysis.
 """
+
+# =============================================================================
+# Imports
+# =============================================================================
+from beamprofiler.niso import characterizing_parameters
+
+__all__ = ['characterizing_parameters']
--- src/beamprofiler/utils/__init__.py
+++ src/beamprofiler/utils/__init__.py
@@ -1,3 +1,10 @@
 """
 This package handles the utilities of the beam analysis.
 """
+
+# =============================================================================
+# Imports
+# =============================================================================
+from beamprofiler.utils import data_processing, plot, report
+
+__all__ = ['data_processing', 'plot', 'report']
